d0ae9dd28f9f0ef9d059e7741ff8ee817747f720
greenarry <96934758+hyeyoung-dev@users.noreply.github.com>
2022-01-16 00:23:37 +0900

    Create BOJ_5554.java

---
+++ Baekjoon Oline Judge/BOJ_5554/BOJ_5554.java
@@ -0,0 +1,21 @@
+// 백준 5554번: 심부름 가는 길
+
+import java.util.Scanner;
+
+public class BOJ_5554 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		int d = scanner.nextInt();
+		
+		int sum = a+b+c+d;
+		
+		if(60 <= sum && sum < 3600) {
+			System.out.println(sum/60);
+			System.out.println(sum%60);
+		}
+	}
+}
