4ea9318e685b7567f6def488de8a1e687e39f27d
greenarry <96934758+hyeyoung-dev@users.noreply.github.com>
2022-01-16 01:11:36 +0900

    Update BOJ_3003.java

--- Baekjoon Oline Judge/BOJ_3003/BOJ_3003.java
+++ Baekjoon Oline Judge/BOJ_3003/BOJ_3003.java
@@ -1,11 +1,21 @@
-// 백준 3003번: 킹, 퀸, 룩, 비숍, 나이트, 폰
-
 import java.util.Scanner;
 
 public class BOJ_3003 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-	}
+		int kg = scanner.nextInt();
+		int qn = scanner.nextInt();
+		int lk = scanner.nextInt();
+		int bp = scanner.nextInt();
+		int kt = scanner.nextInt();
+		int pn = scanner.nextInt();
 
+		System.out.print(1-kg +" ");
+		System.out.print(1-qn +" ");
+		System.out.print(2-lk +" ");
+		System.out.print(2-bp +" ");
+		System.out.print(2-kt +" ");
+		System.out.print(8-pn);
+	}
 }
