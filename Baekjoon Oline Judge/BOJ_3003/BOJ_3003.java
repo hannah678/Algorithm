fabca4b2c660c2e3a4a1b734e6f0bdd50e8bc665
greenarry <96934758+hyeyoung-dev@users.noreply.github.com>
2022-01-16 00:23:34 +0900

    Create BOJ_3003.java

---
+++ Baekjoon Oline Judge/BOJ_3003/BOJ_3003.java
@@ -0,0 +1,11 @@
+// 백준 3003번: 킹, 퀸, 룩, 비숍, 나이트, 폰
+
+import java.util.Scanner;
+
+public class BOJ_3003 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+	}
+
+}
