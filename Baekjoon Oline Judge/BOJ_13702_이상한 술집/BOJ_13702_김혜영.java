b932e18a823a37137b2da19ffc0ff6761e9553b6
Hyeyoung Kim <hyeyoung.dev@gmail.com>
2022-02-22 00:51:51 +0900

    백준 13702번: 이상한 술집 풀이(자바java)

---
+++ Baekjoon Oline Judge/BOJ_13702_이상한 술집/BOJ_13702_김혜영.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//백준 13702번: 이상한 술집
+public class BOJ_13702_김혜영 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		//key point: 제일 큰 막걸리 용량을 몇으로 나눌지
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		//배열 생성
+		long[] arr = new long[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		insertSort(arr);
+		
+		int num=0;
+		long mak=0;
+		int i=1;
+		
+		while(num<K) {
+			num=i;
+			mak=arr[0]/i;
+			for(int j=1; j<N; j++) {
+				num += arr[j]/mak;
+			}
+			i++;
+		}
+		System.out.println(mak);	
+	}
+	private static void insertSort(long[] array) {
+		long temp = 0;
+		int j = 0;
+		
+		for(int i=1; i<array.length; i++){
+			temp = array[i];
+			j = i;
+			while(j > 0 && temp > array[j-1]){ 
+				array[j] = array[j-1]; 
+				j--; 
+			}
+			array[j] = temp;
+			
+		}
+	}
+}
