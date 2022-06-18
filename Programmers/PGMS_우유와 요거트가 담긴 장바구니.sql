04dbe7517111abbc1d62785e780356e9dcae89ad
Hyeyoung Kim <96934758+hyeyoung-dev@users.noreply.github.com>
2022-06-19 03:00:33 +0900

    프로그래머스 LEVEL4 - 우유와 요거트가 담긴 장바구니(MYSQL)

--- Programmers/PGMS_우유와 요거트가 담긴 장바구니.sql
+++ Programmers/PGMS_우유와 요거트가 담긴 장바구니.sql
@@ -1,4 +1,5 @@
--- #문제 : 우유와 요거트가 담긴 장바구니(https://programmers.co.kr/learn/courses/30/lessons/62284)
+-- https://programmers.co.kr/learn/courses/30/lessons/62284
+
 -- 우유(Milk)와 요거트(Yogurt)를 동시에 구입한 장바구니가 있는지 알아보려 합니다. 
 -- 우유와 요거트를 동시에 구입한 장바구니의 아이디를 조회하는 SQL 문을 작성해주세요. 
 -- 이때 결과는 장바구니의 아이디 순으로 나와야 합니다. 
